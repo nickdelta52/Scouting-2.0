b37eca3e115e5e8dc76dddf432466cd0193fc86c
ghana7 <ghana796.2>
2016-02-02 20:39:42 -0500

    Team & Action class

--- Scouting 2.1/Scouting 2.1/Form1.Designer.cs
+++ Scouting 2.1/Scouting 2.1/Form1.Designer.cs
@@ -32,7 +32,10 @@
             this.ptScout = new System.Windows.Forms.TabPage();
             this.ptMatch = new System.Windows.Forms.TabPage();
             this.tbView = new System.Windows.Forms.TabPage();
+            this.lbMatchTeam = new System.Windows.Forms.ListBox();
+            this.LBLMatchTeam = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.ptMatch.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -41,49 +44,75 @@
             this.tabControl1.Controls.Add(this.ptMatch);
             this.tabControl1.Controls.Add(this.tbView);
             this.tabControl1.Location = new System.Drawing.Point(-1, 1);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1033, 652);
+            this.tabControl1.Size = new System.Drawing.Size(775, 530);
             this.tabControl1.TabIndex = 0;
             // 
             // ptScout
             // 
-            this.ptScout.Location = new System.Drawing.Point(4, 25);
+            this.ptScout.Location = new System.Drawing.Point(4, 22);
+            this.ptScout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ptScout.Name = "ptScout";
-            this.ptScout.Padding = new System.Windows.Forms.Padding(3);
-            this.ptScout.Size = new System.Drawing.Size(1025, 623);
+            this.ptScout.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ptScout.Size = new System.Drawing.Size(767, 504);
             this.ptScout.TabIndex = 0;
             this.ptScout.Text = "Pit Scouting";
             this.ptScout.UseVisualStyleBackColor = true;
             // 
             // ptMatch
             // 
-            this.ptMatch.Location = new System.Drawing.Point(4, 25);
+            this.ptMatch.Controls.Add(this.LBLMatchTeam);
+            this.ptMatch.Controls.Add(this.lbMatchTeam);
+            this.ptMatch.Location = new System.Drawing.Point(4, 22);
+            this.ptMatch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ptMatch.Name = "ptMatch";
-            this.ptMatch.Padding = new System.Windows.Forms.Padding(3);
-            this.ptMatch.Size = new System.Drawing.Size(1025, 623);
+            this.ptMatch.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ptMatch.Size = new System.Drawing.Size(767, 504);
             this.ptMatch.TabIndex = 1;
             this.ptMatch.Text = "Match scouting";
             this.ptMatch.UseVisualStyleBackColor = true;
             // 
             // tbView
             // 
-            this.tbView.Location = new System.Drawing.Point(4, 25);
+            this.tbView.Location = new System.Drawing.Point(4, 22);
+            this.tbView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbView.Name = "tbView";
-            this.tbView.Size = new System.Drawing.Size(1025, 623);
+            this.tbView.Size = new System.Drawing.Size(767, 504);
             this.tbView.TabIndex = 2;
             this.tbView.Text = "View Results";
             this.tbView.UseVisualStyleBackColor = true;
             // 
+            // lbMatchTeam
+            // 
+            this.lbMatchTeam.FormattingEnabled = true;
+            this.lbMatchTeam.Location = new System.Drawing.Point(9, 27);
+            this.lbMatchTeam.Name = "lbMatchTeam";
+            this.lbMatchTeam.Size = new System.Drawing.Size(60, 95);
+            this.lbMatchTeam.TabIndex = 0;
+            // 
+            // LBLMatchTeam
+            // 
+            this.LBLMatchTeam.AutoSize = true;
+            this.LBLMatchTeam.Location = new System.Drawing.Point(10, 11);
+            this.LBLMatchTeam.Name = "LBLMatchTeam";
+            this.LBLMatchTeam.Size = new System.Drawing.Size(37, 13);
+            this.LBLMatchTeam.TabIndex = 1;
+            this.LBLMatchTeam.Text = "Team:";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1032, 653);
+            this.ClientSize = new System.Drawing.Size(774, 531);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Scouting 2.1";
             this.tabControl1.ResumeLayout(false);
+            this.ptMatch.ResumeLayout(false);
+            this.ptMatch.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -94,6 +123,8 @@
         private System.Windows.Forms.TabPage ptScout;
         private System.Windows.Forms.TabPage ptMatch;
         private System.Windows.Forms.TabPage tbView;
+        private System.Windows.Forms.Label LBLMatchTeam;
+        private System.Windows.Forms.ListBox lbMatchTeam;
     }
 }
 
