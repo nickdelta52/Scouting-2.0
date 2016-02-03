ce2c04a0478a4d570dffbf9460cffd156eba5652
nickdelta52 <nfbdiamond@gmail.com>
2016-02-02 20:10:12 -0500

    stuff

--- Scouting 2.1/Scouting 2.1/Form1.Designer.cs
+++ Scouting 2.1/Scouting 2.1/Form1.Designer.cs
@@ -29,51 +29,51 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.ptScout = new System.Windows.Forms.TabPage();
+            this.ptMatch = new System.Windows.Forms.TabPage();
+            this.tbView = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.ptScout);
+            this.tabControl1.Controls.Add(this.ptMatch);
+            this.tabControl1.Controls.Add(this.tbView);
             this.tabControl1.Location = new System.Drawing.Point(-1, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1033, 652);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // ptScout
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1025, 623);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.ptScout.Location = new System.Drawing.Point(4, 25);
+            this.ptScout.Name = "ptScout";
+            this.ptScout.Padding = new System.Windows.Forms.Padding(3);
+            this.ptScout.Size = new System.Drawing.Size(1025, 623);
+            this.ptScout.TabIndex = 0;
+            this.ptScout.Text = "Pit Scouting";
+            this.ptScout.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // ptMatch
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1025, 623);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.ptMatch.Location = new System.Drawing.Point(4, 25);
+            this.ptMatch.Name = "ptMatch";
+            this.ptMatch.Padding = new System.Windows.Forms.Padding(3);
+            this.ptMatch.Size = new System.Drawing.Size(1025, 623);
+            this.ptMatch.TabIndex = 1;
+            this.ptMatch.Text = "Match scouting";
+            this.ptMatch.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // tbView
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1025, 623);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tbView.Location = new System.Drawing.Point(4, 25);
+            this.tbView.Name = "tbView";
+            this.tbView.Size = new System.Drawing.Size(1025, 623);
+            this.tbView.TabIndex = 2;
+            this.tbView.Text = "View Results";
+            this.tbView.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -91,9 +91,9 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage ptScout;
+        private System.Windows.Forms.TabPage ptMatch;
+        private System.Windows.Forms.TabPage tbView;
     }
 }
 
